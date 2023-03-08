efff2e49e175a048c5aaba26a2e41ddbf0927e40
kimfield98 <kimfield98@naver.com>
2023-03-08 22:36:05 +0900

    TIL0308

---
+++ python/02.py
@@ -0,0 +1,26 @@
+#1
+a = int(input())
+print(a+2)
+# input은 항상 문자형으로 입력받게 됨. 따라서 필요에 따라 정수형 등으로 바꿔주는 작업이 필요함.
+
+#2
+n = input()
+print(f"Your score is {n} point.")
+# f-string 없이 입력했는데 그냥 붙지 않음.
+
+#3
+a = int(input())
+print(a*2)
+# 정수형으로 변환하였기 때문에 
+
+#4
+a = int(input())
+print(a*2+3)
+#연산가능
+
+#5
+n = float(input())
+print(f"{n:.2f}")
+# 반올림하는거 f안썼네
+
+#6
